664d730d1bc25b31b8b1b77078722165e71adb4f
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-08-19 16:03:06 +0300

    new commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('autoprefixer', function() {
+    gulp.src('app/scss/**/*.scss')
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('scss', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+
+
+gulp.task('script', function() {
+    return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    })
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/*.js', gulp.parallel('script'))
+
+});
+
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'watch'))
+
